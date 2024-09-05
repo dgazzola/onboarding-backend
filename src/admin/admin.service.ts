fc00cc6474b260b0cd61728b7e6b8003aa9af6f6
Dan Gazzola <danielrgazzola@gmail.com>
2024-09-05 17:34:34 -0400

    implemented admin seed functino in read

--- src/admin/admin.service.ts
+++ src/admin/admin.service.ts
@@ -1,21 +1,24 @@
 import { Admin } from "./models/admin.models";
 
 export class AdminService {
-  // Function to login or create a new user
   async read() {
+    let admin = await Admin.findOne();
 
-    // Find user by email
-    const admin = await Admin.findOne();
-    // console.log('service found admin:', admin)
-    return admin
+    if (!admin) {
+      admin = new Admin({
+        aboutme: 3,
+        address: 2,
+        birthdate: 2,
+      });
+      await admin.save();
+    }
+
+    return admin;
   }
+
   async update(updates: any) { // Consider typing this parameter for better type safety
     try {
-      // Extract the ID from the updates object
       const { _id, ...updateData } = updates;
-      console.log('_id:', _id, 'updateData:', updateData)
-
-      // Find and update the document by its ID
       const updatedAdmin = await Admin.findByIdAndUpdate(
         _id, // ID of the document to update
         updateData, // Fields to update
