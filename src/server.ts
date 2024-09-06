1eb0fc8ea512076380ef0feb0a7812a6aaa14163
Dan Gazzola <danielrgazzola@gmail.com>
2024-09-05 20:38:03 -0400

    implementing cors for heroku functionality

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,33 @@
 import express, { Application, Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
-import { connect, disconnect } from './database';
+import { connect } from './database';
 import { userRouter } from './user/user.router';
 import { adminRouter } from './admin/admin.router';
 
-
 const app: Application = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 
-connect();
+const isProduction = process.env.NODE_ENV === 'production';
 
-app.use(cors());
+const corsOptions = isProduction
+  ? {
+      origin: 'https://gazzola-onboard-frontend-968f4888cbbf.herokuapp.com',
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+      credentials: true,
+    }
+  : {
+      origin: '*',
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+      credentials: true,
+    };
+
+app.use(cors(corsOptions));
+
 app.use(express.json());
 app.use(cookieParser());
+
+connect();
 
 app.use('/user', userRouter);
 app.use('/admin', adminRouter);
